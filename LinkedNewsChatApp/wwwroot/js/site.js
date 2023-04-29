17031e7c6d4f24f34f9ebedc654e0d930cb1359e
ViktorKlub <globator20016@gmail.com>
2023-04-29 14:58:20 +0300

    Зробив Скрол при проходженні висоти 235px

--- LinkedNewsChatApp/wwwroot/js/site.js
+++ LinkedNewsChatApp/wwwroot/js/site.js
@@ -2,20 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("widget").classList.add("show");
-    } else {
-        document.getElementById("widget").classList.remove("show");
-    }
-}
 
-function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 
 function footer() {
@@ -28,3 +16,24 @@ function footer() {
 
 const container = document.querySelector('.container-fluid');
 container.classList.add('active');
+
+function scrollFunction() {
+    if (document.body.scrollTop >235 || document.documentElement.scrollTop >235) {
+        document.getElementById("my_Btn").style.display = "block";
+    } else {
+        document.getElementById("my_Btn").style.display = "none";
+    }
+}
+
+// Викликаємо функцію scrollFunction() при кожному скролі сторінки
+window.onscroll = function () {
+    scrollFunction();
+};
+
+// Визначаємо функцію topFunction(), яка буде прокручувати сторінку до верху,
+// коли користувач клікає на кнопку
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
